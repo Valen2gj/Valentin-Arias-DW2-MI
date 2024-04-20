155dde10048898a39121dc03ff7557fe99e49e34
Valen2gj <2020valentin.arias@gmail.com>
2024-04-20 09:23:56 -0300

    Desafio2

---
+++ backend/script.js
@@ -0,0 +1,5 @@
+let nombreProducto = "labial";
+let precioUnitario = 8750;
+let cantidadDeseada = prompt("Ingrese la cantidad deseada");
+let precioTotal = precioUnitario * cantidadDeseada;
+alert(precioTotal)
