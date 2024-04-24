4b21f85decac87324a9888e3b39fd51ceaf16e64
Valen2gj <2020valentin.arias@gmail.com>
2024-04-24 19:59:23 -0300

    Desafio3

--- backend/script.js
+++ backend/script.js
@@ -1,5 +1,14 @@
 let nombreProducto = "labial";
 let precioUnitario = 8750;
 let cantidadDeseada = prompt("Ingrese la cantidad deseada");
-let precioTotal = precioUnitario * cantidadDeseada;
-alert(precioTotal)
+if (cantidadDeseada >= 5){
+    let precioTotal = precioUnitario * cantidadDeseada
+    let descuento = precioTotal * 0.10
+    let precioDescuento = precioTotal - descuento
+    alert("El total es de: $" + precioDescuento)
+}
+else{
+    let precioTotal = precioUnitario * cantidadDeseada;
+    alert("El total es de: $" + precioTotal)
+
+}
