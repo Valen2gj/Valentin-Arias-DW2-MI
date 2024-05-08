e823e3724f3b2c892f2ba85d2743490293991b4c
Valentin Arias <166662828+Valen2gj@users.noreply.github.com>
2024-05-08 19:45:35 -0300

    Add files via upload

---
+++ Desafio_grupal.js
@@ -0,0 +1,24 @@
+●Registro de usuario:
+-Nombre 
+-Mail
+-Contraseña 
+-Mensaje (Ejemplo: "Se ha registrado exitosamente el usuario ....")
+
+●Inicio de sesión:
+-Mail 
+-Contraseña 
+-Mensaje (Ejemplo: ¡Hola ..., bienvenido a nuestro sitio!)
+
+●Actualizar contraseña:
+-Mail 
+-Contraseña 
+-Nueva contraseña 
+-Mensaje ("Se ha actualizado la contraseña para el correo ....")
+let Nombre; 
+let Mail;
+let Contraseña;
+function registro(){
+    Nombre = prompt("Ingrese su nombre")
+    Mail = prompt("Ingrese su mail")
+    Contraseña = prompt("Ingrese su contraseña")
+}
